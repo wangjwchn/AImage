aecf8322546b2d05f9794992227560bf24fdd2f5
王佳玮 <wangjwchn@wangjiaweideMacBook-Air.local>
2016-03-05 15:35:03 +0800

    Mofify Example file

--- Example/JWAnimatedImage/ViewController.swift
+++ Example/JWAnimatedImage/ViewController.swift
@@ -12,10 +12,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let imageData = NSData(contentsOfURL:NSBundle.mainBundle().URLForResource("test", withExtension: "gif")!)
+        
+        //one GIF image
         let imageView = JWAnimatedImageView()
         imageView.addGifImage(imageData!)
         imageView.frame = CGRect(x: 7.0, y: 50.0, width: 400.0, height: 224.0)
         view.addSubview(imageView)
+        
+        //30 GIF images
+        /*
+        for i in 0..<3 {
+            for j in 0..<10 {
+                let imageView = JWAnimatedImageView()
+                imageView.addGifImage(imageData!)
+                imageView.frame = CGRect(x: 0.0+Double(100*i), y:50.0+Double(56*j), width: 100.0, height: 56.0)
+                view.addSubview(imageView)
+            }
+        }
+    */
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
