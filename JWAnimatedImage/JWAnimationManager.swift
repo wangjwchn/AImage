32d3308e364984634646f09286d97cae691f7532
王佳玮 <wangjwchn@wangjiaweideMacBook-Air.local>
2016-03-29 09:26:22 +0800

    Fix #11

--- JWAnimatedImage/JWAnimationManager.swift
+++ JWAnimatedImage/JWAnimationManager.swift
@@ -16,7 +16,7 @@ public class JWAnimationManager{
     private var displayViews:[UIImageView] = []
     private var totalGifSize:Int
     private var memoryLimit:Int
-    public var haveCache:Bool
+    public var  haveCache:Bool
     
     public init(memoryLimit:Int){
         self.memoryLimit = memoryLimit
@@ -24,19 +24,15 @@ public class JWAnimationManager{
         self.haveCache = true
         self.timer = CADisplayLink(target: self, selector: #selector(self.UpdateImageView))
         self.timer!.addToRunLoop(.mainRunLoop(), forMode: NSRunLoopCommonModes)
-        
     }
     
     public func AddImageView(imageView:UIImageView){
         self.totalGifSize+=imageView.gifImage.imageSize!
-        //print("Total memory usage in Manager: \(self.totalGifSize)")
         if(self.totalGifSize>memoryLimit&&self.haveCache==true){
             self.haveCache = false
             for imageView in self.displayViews{
                 dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH,0)){
-                    if(self.CheckForCache(imageView)==false){
-                    imageView.changetoNOCacheMode()
-                    }
+                    imageView.CheckCache()
                 }
             }
         }
@@ -48,14 +44,11 @@ public class JWAnimationManager{
         if let index = self.displayViews.indexOf(imageView){
             self.displayViews.removeAtIndex(index)
             self.totalGifSize-=imageView.gifImage.imageSize!
-            //print("Total memory usage in Manager: \(self.totalGifSize)")
             if(self.totalGifSize<memoryLimit&&self.haveCache==false){
                 self.haveCache = true
                 for imageView in self.displayViews{
                     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH,0)){
-                        if(self.CheckForCache(imageView)==true){
-                        imageView.changetoCacheMode()
-                        }
+                        imageView.CheckCache()
                     }
                 }
             }
@@ -67,8 +60,9 @@ public class JWAnimationManager{
     }
     
     public func CheckForCache(imageView:UIImageView) ->Bool{
+        if(imageView.displaying == false){return false}
         if(imageView.loopTime == -1||imageView.loopTime >= 5){
-        return self.haveCache
+            return self.haveCache
         }else{
             return false
         }
@@ -77,10 +71,10 @@ public class JWAnimationManager{
     @objc func UpdateImageView(){
         for imageView in self.displayViews{
             dispatch_async(dispatch_get_main_queue()){
-            imageView.image = imageView.currentImage
+                imageView.image = imageView.currentImage
             }
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH,0)){
-                imageView.updateCurrentImage(self)
+                imageView.updateCurrentImage()
             }
         }
     }
