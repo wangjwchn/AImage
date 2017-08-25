5d7618f24b8cf8a7ff12625532496c1fc7af819f
wangjwchn <wangjwchn@yahoo.com>
2017-08-25 15:51:17 +0800

    update to swift 4

---
+++ AImage.swift
@@ -0,0 +1,243 @@
+//
+//  AImage.swift
+//  AImage
+//
+//  Created by wangjwchn on 16/8/22.
+//  Copyright © 2016 JW. All rights reserved.
+//
+
+/********************* Demo *********************
+
+/* Create AImage with URL */
+let image = AImage(url: Bundle.main.url(forResource: "test", withExtension: "gif")!)
+
+/* Create AImageView */
+let imageview = AImageView(frame:CGRect(x: 0.0, y: 50.0, width: 380.0, height: 212.0))
+
+/* Add AImage to AImageView */
+imageview.add(image: image!)
+
+/* Start displaying animated image */
+imageview.play = true
+    
+    ...
+    ...
+        
+/* Stop displaying animated image */
+imageview.play = false
+
+********************* Demo *********************/
+
+import ImageIO
+import UIKit
+
+public class AImageView: UIView {
+    
+    /* Whether the image is displaying or not */
+    public var play: Bool = false
+    
+    /* Add an 'AImage' to 'AImageView' */
+    // limit: Memory limit (in MB)
+    public func add(image: AImage, limit: Int = 20){
+        clear()
+        self.aImage = image
+        self.nextFrame = AImageView.cover(self.aImage!.imageSource)
+        if (AImageView.calculateMemory(self.aImage!.imageSource) <= limit) {
+            DispatchQueue.global(qos: DispatchQoS.QoSClass.userInteractive).async {
+                self.prepareCache()
+            }
+        }
+        self.timer = CADisplayLink(target: self.displayLinkProxy, selector: #selector(myDisplayLinkProxyObject.proxyUpdateAnimation))
+        timer?.preferredFramesPerSecond = self.aImage!.framePerSecond
+        timer?.add(to: RunLoop.main, forMode: .commonModes)
+    }
+    
+    deinit {
+        timer?.invalidate()
+    }
+    
+    private var aImage:AImage?
+    private var indexAt:Int = 0
+    private var timer:CADisplayLink?
+    private var nextFrame:UIImage?
+    private var cache:NSCache<AnyObject, AnyObject> = NSCache()
+    private lazy var displayLinkProxy: myDisplayLinkProxyObject = {
+        return myDisplayLinkProxyObject(listener: self)
+    }()
+    
+    private class func cover(_ source: CGImageSource) -> UIImage {
+        return UIImage(cgImage: CGImageSourceCreateImageAtIndex(source,0,nil)!)
+    }
+    
+    private class func calculateMemory(_ source: CGImageSource) -> Int {
+        let imageCount = CGImageSourceGetCount(source)
+        let image = cover(source)
+        return Int(image.size.height * image.size.width * 4) * imageCount / (1000 * 1000)
+    }
+    
+    private func clear(){
+        self.indexAt = 0
+        self.cache.removeAllObjects()
+        self.play = false
+        if self.timer != nil {
+            timer?.invalidate()
+            self.timer = nil
+        }
+    }
+    
+    private func prepareCache() {
+        for i in 0..<self.aImage!.displayIndex.count {
+            let key = self.aImage!.displayIndex[i]
+            if (self.cache.object(forKey: key as NSNumber) == nil) {
+                let image = decodeImage(source: self.aImage!.imageSource, index: key)
+                self.cache.setObject(image,forKey:key as NSNumber)
+            }
+        }
+    }
+    
+    private func decodeImage(source: CGImageSource,index: Int) -> UIImage {
+        let image = UIImage(cgImage: CGImageSourceCreateImageAtIndex(source,index,nil)!)
+        UIGraphicsBeginImageContext(CGSize(width: image.size.width, height: image.size.height))
+        image.draw(in: CGRect(x: 0, y: 0,width: image.size.width, height: image.size.height))
+        let rawImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return rawImage!
+    }
+    
+    internal func timerFired() {
+        self.layer.contents = self.nextFrame?.cgImage
+        let nextAt = ((self.indexAt)+1)%(self.aImage!.displayIndex.count)
+        if (self.aImage!.displayIndex [nextAt] != self.aImage!.displayIndex [(self.indexAt)]) {
+            DispatchQueue.global(qos: DispatchQoS.QoSClass.userInteractive).async {
+                let key = self.aImage!.displayIndex[nextAt]
+                self.nextFrame = self.cache.object(forKey: key as AnyObject) as? UIImage
+                if (self.nextFrame == nil){
+                    self.nextFrame = self.decodeImage(source: self.aImage!.imageSource, index: key)
+                }
+            }
+        }
+        if (self.play == true){
+            self.indexAt = nextAt
+            if (self.indexAt == self.aImage!.displayIndex.count - 1) {
+                self.aImage!.loopCount -= 1
+                if (self.aImage!.loopCount == 0) {
+                    self.play = false
+                }
+            }
+        }
+    }
+}
+
+public class AImage {
+    
+    fileprivate let imageSource: CGImageSource
+    fileprivate var framePerSecond:Int
+    fileprivate var displayIndex:[Int]
+    fileprivate var loopCount:Int
+    
+    /* Create an 'AImage' from URL */
+    //quality: display quality, 1 is best, 0 is worst
+    //loop: display time, -1 means forever
+    convenience init?(url: URL, quality: Float = 1.0, loop: Int = -1) {
+        guard let src = CGImageSourceCreateWithURL(url as CFURL, nil) else {
+            return nil
+        }
+        self.init(source: src, quality, loop)
+    }
+    
+    /* Create an 'AImage' from Data */
+    //quality: display quality, 1 is best, 0 is worst
+    //loop: display time, -1 means forever
+    convenience init?(data: Data, quality: Float = 1.0, loop: Int = -1) {
+        guard let src = CGImageSourceCreateWithData(data as CFData, nil) else {
+            return nil
+        }
+        self.init(source: src, quality, loop)
+    }
+    
+    private init(source: CGImageSource, _ quality: Float, _ loop: Int) {
+        self.imageSource = source
+        var frameDelays = AImage.calcuDelayTimes(imageSource:source)
+        (self.framePerSecond,self.displayIndex) = AImage.calculateFrameDelay(&frameDelays, quality)
+        self.loopCount = loop
+    }
+    
+    private class func calcuDelayTimes(imageSource: CGImageSource) -> [Float] {
+        
+        let frameCount = CGImageSourceGetCount(imageSource)
+        var imageProperties = [CFDictionary]()
+        for i in 0..<frameCount {
+            imageProperties.append(CGImageSourceCopyPropertiesAtIndex(imageSource, i, nil)!)
+        }
+        
+        var frameProperties = [CFDictionary]()
+        if (CFDictionaryContainsKey(imageProperties[1],
+            Unmanaged.passUnretained(kCGImagePropertyGIFDictionary).toOpaque())) {
+            frameProperties = imageProperties.map() {
+                unsafeBitCast(CFDictionaryGetValue($0, Unmanaged.passUnretained(kCGImagePropertyGIFDictionary).toOpaque()), to: CFDictionary.self)
+            }//gif
+        } else if (CFDictionaryContainsKey(imageProperties[1],
+            Unmanaged.passUnretained(kCGImagePropertyPNGDictionary).toOpaque())) {
+            frameProperties = imageProperties.map() {
+                unsafeBitCast(CFDictionaryGetValue($0,
+                   Unmanaged.passUnretained(kCGImagePropertyPNGDictionary).toOpaque()),to: CFDictionary.self)
+            }//apng
+        } else {
+            fatalError("Illegal image type.")
+        }
+
+        let frameDelays: [Float] = frameProperties.map() {
+            var delayObject: AnyObject = unsafeBitCast(CFDictionaryGetValue($0, Unmanaged.passUnretained(kCGImagePropertyGIFUnclampedDelayTime).toOpaque()), to: AnyObject.self)
+            if (delayObject.floatValue == 0.0){
+                delayObject = unsafeBitCast(CFDictionaryGetValue($0, Unmanaged.passUnretained(kCGImagePropertyGIFDelayTime).toOpaque()), to: AnyObject.self)
+            }
+            return delayObject as! Float
+        }
+        return frameDelays
+    }
+    
+    private class func calculateFrameDelay(_ delays: inout [Float],_ quality: Float) -> (Int,[Int]) {
+        
+        let framePerSecondChoices = [1,2,3,4,5,6,10,12,15,20,30,60]
+        let displayRefreshDelayTime = framePerSecondChoices.map{ 1.0 / Float($0) }
+        for i in 1..<delays.count {
+            delays[i] += delays[i-1]
+        }
+        
+        var order: [Int] = [Int]()
+        var fps: Int = framePerSecondChoices.last!
+        for i in 0..<framePerSecondChoices.count {
+            let displayPosition = delays.map{ Int($0 / displayRefreshDelayTime[i]) }
+            var framelosecount = 0
+            for j in 1..<displayPosition.count {
+                if (displayPosition[j] == displayPosition[j-1])
+                {framelosecount += 1}
+            }
+            if (Float(framelosecount) <= Float(displayPosition.count) * (1 - quality) || i == displayRefreshDelayTime.count - 1) {
+                fps = framePerSecondChoices[i]
+                var indexOfold = 0, indexOfnew = 1
+                while (indexOfnew <= displayPosition.last!) {
+                    if (indexOfnew <= displayPosition[indexOfold]) {
+                        order.append(indexOfold)
+                        indexOfnew += 1
+                    } else {
+                        indexOfold += 1
+                    }
+                }
+                break
+            }
+        }
+        return (fps,order)
+    }
+}
+
+// Use a proxy object to break the CADisplayLink retain cycle
+fileprivate class myDisplayLinkProxyObject {
+    weak var myListener: AImageView?
+    init(listener: AImageView) {
+        myListener = listener
+    }
+    @objc func proxyUpdateAnimation(link: CADisplayLink) {
+        myListener?.timerFired()
+    }
+}
--- README.md
+++ README.md
@@ -1,125 +1,69 @@
-![Cover](https://raw.githubusercontent.com/wangjwchn/BenchmarkImage/master/Cover.png)
+![Cover](https://wangjwchn.github.io/image/cover.png)
 
-[![Language](https://img.shields.io/badge/swift-2.3-orange.svg)](http://swift.org)
-[![Build Status](https://travis-ci.org/wangjwchn/AImage.svg?branch=master)](https://travis-ci.org/wangjwchn/AImage)
-[![CocoaPods Compatible](https://img.shields.io/cocoapods/v/AImage.svg)](https://img.shields.io/cocoapods/v/AImage.svg)
-[![Carthage compatible](https://img.shields.io/badge/Carthage-compatible-4BC51D.svg?style=flat)](https://github.com/Carthage/Carthage)
+[![Language](https://img.shields.io/badge/swift-4.0-orange.svg)](http://swift.org)
 [![Pod License](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/wangjwchn/AImage/master/LICENSE)
 
-An animated gif & apng engine for iOS in Swift with low memory & cpu usage.
-
-![video](http://i.imgur.com/XOoq9mP.gif)
 
 ## Features
 
-- [x] Small but complete,only `250` lines of code. 
-- [x] As `UIImage` and `UIImageView` extension,easy to use.
-- [x] Allow to control display quality, memory usage and display progress.
-- [x] Optimized for multi-image case.
-- [x] Have a great performance on memory and cpu usage.Using asynchronous image decoding to reduce the main thread CPU usage.
+- [x] Small but complete, only`200`lines of code. 
+- [x] Allow to control display quality, memory usage, loop time and display progress.
+- [x] Have a great performance on memory and cpu usage. 
+- [x] Using asynchronous image decoding to reduce the main thread CPU usage.
 
-
-## Installation
-
-#### CocoaPods
-You can use [CocoaPods](http://cocoapods.org/) to install `AImage` by adding it to your `Podfile`:
-
-```ruby
-platform :ios, '8.0'
-use_frameworks!
-pod 'AImage'
-```
-
-To get the full benefits import `AImage` wherever you import UIKit
-
-``` swift
-import UIKit
-import AImage
-```
-
-#### Carthage
-Create a `Cartfile` that lists the framework and run `carthage bootstrap`. Follow the [instructions](https://github.com/Carthage/Carthage#if-youre-building-for-ios) to add `$(SRCROOT)/Carthage/Build/iOS/AImage.framework` to an iOS project.
-
-```
-github "wangjwchn/AImage"
-```
-#### Manually
-1. Download and drop ```/AImage```folder in your project.  
-2. Congratulations!  
 
 ## Usage
 
 ```swift
-/* Load gif or apng image as NSData */
-let imageData = NSData(contentsOfURL:NSBundle.mainBundle().URLForResource("test", withExtension: "gif")!)
+/* Create AImage with URL */
+let image = AImage(url: Bundle.main.url(forResource: "test", withExtension: "gif")!)
 
-/*  Pass NSData into UIImage  */
-let image = UIImage(AImageData:imageData!)
+/* Create AImageView */
+let imageview = AImageView(frame:CGRect(x: 0.0, y: 50.0, width: 380.0, height: 212.0))
 
-/* Pass UIImage into UIImageView */
-let imageview = UIImageView(AImage: image)
-
-/* Add view to root view */
-imageview.frame = CGRect(x: 7.0, y: 50.0, width: 400.0, height: 224.0)
-view.addSubview(imageview)
+/* Add AImage to AImageView */
+imageview.add(image: image!)
 
 /* Start displaying animated image */
-imageview.APlay();
+imageview.play = true
 
-...
 ...
 ...
 
 /* Stop displaying animated image */
-iamgeview.AStop();
+imageview.play = false
 
 ```
 
-## Principles
-- [Here](https://wangjwchn.github.io/blog/Display-animated-image-on-iOS-device)
-
 ## Benchmark
-### Display GIF:Compared with [FLAnimatedImage](https://github.com/Flipboard/FLAnimatedImage)
-##### 1.Display 1 Image
-|               |CPU Usage[average] |Memory Usage[average]/MB |
-|:-------------:|:-----------------:|:-----------------------:|
-|AImage|6% ~ 14% [8%]      |7.5 ~ 8.4 [8.2]          |
-|FLAnimatedImage|8% ~ 24% [11%]     |7.3 ~ ??? [???]          |
 
-##### 2.Display 3 Images
-|               |CPU Usage[average] |Memory Usage[average]/MB |
-|:-------------:|:-----------------:|:-----------------------:|
-|AImage|31% ~ 44% [38%]    |12.4 ~ 13.4 [12.9]       |
-|FLAnimatedImage|36% ~ 62% [54%]    |11.0 ~ 12.4 [11.3]       |
+Compared with [Gifu](https://github.com/kaishin/Gifu/tree/swift4) and [Apple's example code](https://developer.apple.com/library/content/samplecode/UsingPhotosFramework/Introduction/Intro.html).
 
-##### 3.Display 30 Images
-|               |CPU Usage[average] |Memory Usage[average]/MB |
-|:-------------:|:-----------------:|:-----------------------:|
-|AImage|38% ~ 81% [53%]    |59.3 ~ 82.4 [63.3]       |
-|FLAnimatedImage|126% ~ 185% [143%] |58.4 ~ 98.9 [74.2]       |
+#### Test1: Display [view.gif](https://wangjwchn.github.io/image/view.gif)
 
+|Library|CPU|Memory|
+|:--:|:--:|:--:|
+|Apple's Example Code |  <img src="https://wangjwchn.github.io/image/apple-view-cpu.png" width = "1000" height = "150" />   |    <img src="https://wangjwchn.github.io/image/apple-view-mem.png" width = "600" height = "120" />     |
+|Gifu| <img src="https://wangjwchn.github.io/image/gifu-view-cpu.png" width = "1000" height = "150" />   |    <img src="https://wangjwchn.github.io/image/gifu-view-mem.png" width = "600" height = "120" />     |
+|AImage|  <img src="https://wangjwchn.github.io/image/aimage-view-cpu.png" width = "1000" height = "150" />   |    <img src="https://wangjwchn.github.io/image/aimage-view-mem.png" width = "600" height = "120" />     |
 
-### Display APNG:Compared with [APNGKit](https://github.com/onevcat/APNGKit)
+#### Test2: Display [view.gif](https://wangjwchn.github.io/image/earth.gif)
 
-#### 1.Display 1 Image
-|               				|CPU Usage[average] |Memory Usage[average]/MB |
-|:------------------------:|:-----------------:|:-----------------------:|
-|AImage (Cache)	|2% ~ 44% [3%]      |43.2 ~ 43.2 [43.2]       |
-|AImage (noCache)	|20% ~ 49% [33%]    |6.6 ~ 8.3 [7.5]          |
-|APNGKit (Cache)				|1% ~ 42% [1%]      |95.6 ~ 95.6 [95.6]        |
-|APNGKit (noCache)			|1% ~ 26% [1%]      |95.9 ~ 95.9 [95.9]        |
-
+|Library|CPU|Memory|
+|:--:|:--:|:--:|
+|Apple's Example Code |  <img src="https://wangjwchn.github.io/image/apple-earch-cpu.png" width = "1000" height = "150" />   |    <img src="https://wangjwchn.github.io/image/apple-earth-mem.png" width = "600" height = "120" />     |
+|Gifu| <img src="https://wangjwchn.github.io/image/gifu-earth-cpu.png" width = "1000" height = "150" />   |    <img src="https://wangjwchn.github.io/image/gifu-earth-mem.png" width = "600" height = "120" />     |
+|AImage|  <img src="https://wangjwchn.github.io/image/aimage-earth-cpu.png" width = "1000" height = "150" />   |    <img src="https://wangjwchn.github.io/image/aimage-earth-mem.png" width = "600" height = "120" />     |
 
 Measurement Factors:
 
- - Measurement time: April 26, 2016
+ - Measurement time: August 24, 2017
 
- - Measurement device: iPhone6 with iOS 9.3
-
- - Measurement tool: Profile in Xcode 7.3
-
- - Measurement image: See it in repository, all the parameters are default.
+ - Measurement device: iPhone6, iOS 11.0(15A5362a)
  
+## Old Version
+
+Version of swift 2.3 can be found in [here](https://github.com/wangjwchn/AImage/tree/Swift2.3).
  
 ## Licence
 AImage is released under the MIT license. See [LICENSE](https://github.com/wangjwchn/JWAnimatedImage/raw/master/LICENSE) for details.
